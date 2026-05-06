82135bf0915de97b2796f555229be8ed59af4a99
이지수 <leejisu@ijisuui-MacBookAir.local>
2026-05-06 11:56:09 +0900

    Initial commit

---
+++ src/main/java/com/fitmate/fit_mate_server/FitMateServerApplication.java
@@ -0,0 +1,13 @@
+package com.fitmate.fit_mate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitmate/fit_mate_server/FitMateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmate.fit_mate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
